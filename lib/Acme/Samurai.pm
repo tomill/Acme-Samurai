b3ba6075579e6544f91a8abb5d92277e25e0cbc4
Naoki Tomita <tomita@cpan.org>
2016-09-10 17:56:44 +0900

    rewrite with Text::Mecabist (test saves me!

--- lib/Acme/Samurai.pm
+++ lib/Acme/Samurai.pm
@@ -1,138 +1,162 @@
 package Acme::Samurai;
+use 5.010001;
 use strict;
 use warnings;
 use utf8;
 our $VERSION = '0.02';
 
-use base 'Acme::Samurai::Base';
+use File::ShareDir qw/dist_file/;
+use Lingua::JA::Alphabet::Yomi qw/alphabet2yomi/;
+use Lingua::JA::Numbers qw/num2ja/;
+use Unicode::Japanese qw/unijp/;
 
-use Lingua::JA::Alphabet::Yomi;
-use Lingua::JA::Numbers;
-use Unicode::Japanese;
+use Text::Mecabist;
 
 sub gozaru {
-    shift->new->transform(@_);
+    my $self = bless { }, shift;
+    my $text = shift // "";
+
+    my $parser = Text::Mecabist->new({
+        node_format => '%m,%H',
+        unk_format  => '%m,%H',
+        bos_format  => '%m,%H',
+        eos_format  => '%m,%H',
+        userdic     => dist_file('Acme-Samurai', Text::Mecabist->encoding->name . '.dic'),
+    });
+
+    # natukashi
+    $text = unijp($text)->z2hNum->h2zAlpha->getu;
+
+    my $doc = $parser->parse($text, sub {
+        my $node = shift;
+        $self->apply_rules($node);
+    });
+    
+    return $self->finalize($doc);
 }
 
-sub prepare {
-    my $self = shift;
-    my $text = Unicode::Japanese->new($self->text);
-    $text->z2hNum->h2zAlpha;
-    $self->text($text->getu);
+sub apply_rules {
+    my ($self, $node) = @_;
+    
+    return if not $node->readable;
+    
+    my $text = $node->text;
+
+    # one to one custom dictionary
+    if ($node->extra) {
+        $text = $node->extra;
+    }
+    
+    if ($node->is('名詞') or $node->is('記号')) {
+        
+        # arabic number to kanji
+        if ($node->pos1 eq '数' and $node->surface =~ /^[0-9]+$/) {
+            # no 位
+            if ($node->surface =~ /^0/ or
+                $node->prev && $node->prev->surface =~ /[.．]/) {
+                
+                $text = join "", map { num2ja($_) } split //, $node->surface;
+            } else {
+                $text = num2ja($node->surface); # with 位
+            }
+        }
+        
+        # kanji number to more classic
+        elsif ($node->pos1 eq '数') {
+            $text =~ tr{〇一二三四五六七八九十百万}
+                       {零壱弐参四伍六七八九拾佰萬};
+        }
+        
+        # roman
+        elsif ($text =~ /^\p{Latin}+$/) {
+            $text = $node->pronunciation if $node->pronunciation;
+            $text = alphabet2yomi($text, 'en');
+            $text = unijp($text)->kata2hira->getu;
+        }
+    }    
+    
+    if ($node->is('動詞')) {
+        if ($text =~ /(.+?)(じる)$/) {
+            $text = "$1ずる";
+        }
+        if ($text eq 'い' and
+            $node->feature =~ /^動詞,非自立,[*],[*],一段,連用形/ and
+            $node->next and
+            $node->next->pos !~ /詞/) {
+            
+            $text = 'おっ' if $node->next->lemma eq 'た';
+            $text = 'おり' if $node->next->lemma eq 'ます';
+        }
+    }
+
+    if ($node->is('形容詞')) {
+        if ($text =~ /^(.+?)(しい|しく)$/) {
+            $text = $1 . { 'しい' => 'しき', 'しく' => 'しゅう' }->{$2};
+        }
+    }
+    
+    if ($node->is('助詞')) {
+        if ($node->feature eq '助詞,終助詞,*,*,*,*,の,の,の,のか' and
+            $node->prev and
+            $node->prev->surface eq 'な') {
+            $node->prev->skip(1);
+            $text = 'なの';
+        }
+        elsif ($text eq 'ので' and
+            $node->prev and
+            $node->prev->surface eq 'な') {
+            $node->prev->skip(1);
+            $text = 'ゆえに';
+        }
+        elsif ($node->surface eq 'ね' and
+            $node->prev and
+            $node->prev->surface eq 'の') {
+            $text = 'だな';
+        }
+    }
+    
+    if ($node->is('助動詞')) {
+        if ($text eq 'ない') {
+            if ($node->prev and
+                $node->prev->surface eq 'し' and
+                $node->next and
+                $node->next->surface and
+                $node->next->pos !~ /詞/) {
+                $node->prev->skip(1);
+                $text = 'せぬ';
+            }
+            if ($node->prev and
+                $node->prev->surface ne 'し' and
+                $node->prev->inflection_form eq '未然形') {
+                $text = 'ぬ';
+            }
+        }
+        elsif ($text eq 'なけれ') {
+            if ($node->prev and
+                $node->prev->surface eq 'し') {
+                $node->prev->skip(1);
+                $text = 'せね';
+            }
+        }
+    }
+    
+    if ($node->is('感動詞')) {
+        if ($node->next and
+            $node->next->pos !~ /詞/) {
+            $text = $node->extra if $node->extra;
+            $text .= 'でござる';
+        }
+    }
+
+    $node->text($text);
 }
 
 sub finalize {
-    my $self = shift;
-    $self->{text} =~ s/(?:ておりまする|ていまする?)\b/ており候/g;
-    $self->{text} =~ s/(?:どうも)?かたじけない(?:ございま(?:する|す|した))?/かたじけない/g;
-}
-
-sub 名詞_rule {
-    my ($self, $node) = @_;
-    
-    if ($node->features->{extra}) {
-        return $node->features->{extra};
-    }
-    elsif ($node->features->{category1} eq '数' and $node->surface =~ /^[0-9]+$/) { # arabic number
-        if ($node->surface =~ /^0/ or
-            $node->prev_node->surface =~ /[.．]/) {
-            my $r = "";
-            $r .= Lingua::JA::Numbers::num2ja($_) for split //, $node->surface;
-            return $r;
-        } else {
-            return Lingua::JA::Numbers::num2ja($node->surface);
-        }
-    }
-    elsif ($node->features->{category1} eq '数') { # maybe kanji number
-        my $text = $node->surface;
-        $text =~ tr{〇一二三四五六七八九十百万}
-                   {零壱弐参四伍六七八九拾佰萬};
-        return $text;
-    }
-    elsif ($node->surface =~ /^\p{Latin}+$/) {
-        my $text = $node->features->{pronounse} || $node->surface;
-        $text = Lingua::JA::Alphabet::Yomi->alphabet2yomi($text);
-        $text = Unicode::Japanese->new($text)->kata2hira->getu;
-        return $text;
-    }
-    return;
-}
-
-*記号_rule = \&名詞_rule;
-
-sub 動詞_rule {
-    my ($self, $node) = @_;
-    if ($node->surface =~ /(.+?)(じる)$/) {
-        return "$1ずる";
-    }
-    if ($node->surface eq 'い' and
-        $node->feature =~ /^動詞,非自立,[*],[*],一段,連用形/ and
-        $node->next_node->features->{pos} !~ /詞/) {
-        return "おっ" if $node->next_node->features->{original} eq 'た';
-        return "おり" if $node->next_node->features->{original} eq 'ます';
-    }
-    return;
-}
-
-sub 形容詞_rule {
-    my ($self, $node) = @_;
-    if ($node->surface =~ /^(.+?)(しい|しく)$/) {
-        my $a = $1;
-        my $b = { 'しい' => 'しき', 'しく' => 'しゅう' }->{$2};
-        return "$a$b";
-    }
-    return;
-}
-
-sub 助詞_rule {
-    my ($self, $node) = @_;
-    if ($node->feature eq '助詞,終助詞,*,*,*,*,の,の,の,のか' and
-        $node->prev_node->surface eq 'な') {
-        $self->parts_pop;
-        return "なの";
-    }
-    elsif ($node->surface eq 'ので' and
-        $node->prev_node->surface eq 'な') {
-        $self->parts_pop;
-        return "ゆえに";
-    }
-    elsif ($node->surface eq 'ね' and
-        $node->prev_node->surface eq 'の') {
-        return "だな";
-    }
-    return;
-}
-
-sub 助動詞_rule {
-    my ($self, $node) = @_;
-    if ($node->surface eq 'ない') {
-        if ($node->prev_node->surface eq 'し' and
-            $node->next_node->surface and
-            $node->next_node->features->{pos} !~ /詞/) {
-            $self->parts_pop;
-            return "せぬ";
-        }
-        if ($node->prev_node->surface ne 'し' and
-            $node->prev_node->features->{inflect_type} eq '未然形') {
-            return "ぬ";
-        }
-    }
-    elsif ($node->surface eq 'なけれ') {
-        if ($node->prev_node->surface eq 'し') {
-            $self->parts_pop;
-            return "せね";
-        }
-    }
-    return;
-}
-
-sub 感動詞_rule {
-    my ($self, $node) = @_;
-    if ($node->next_node->features->{pos} !~ /詞/) {
-        my $text = $node->features->{extra} || $node->surface;
-        return $text . "でござる";
-    }
-    return;
+    my ($self, $doc) = @_;
+    my $text = $doc->join('text');
+    $text =~ s/(?:ておりまする|ていまする?)\b/ており候/g;
+    $text =~ s/(?:どうも)?かたじけない(?:ございま(?:する|す|した))?/かたじけない/g;
+    $text;
 }
 
 1;
